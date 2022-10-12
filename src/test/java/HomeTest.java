8826439af19afd3ec6cc3b5376c4a2fb6c47cbc2
saleela chanyutthakul <saleelachanyutthakul@saleelas-mbp.myfiosgateway.com>
2022-10-12 06:27:04 -0400

    add HomeTest US002

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,3 +1,6 @@
+import jdk.incubator.vector.VectorOperators;
+import org.testng.annotations.Test;
+
 public class HomeTest {
     String name = "Saleela";
     String role = "Student";
@@ -6,5 +9,10 @@ public class HomeTest {
     int phone = 222-222-2222;
     String address = "555 25nd SE Washington DC 200003";
     int num = 1234567;
+
+    @Test(testName = "test US002")
+    public void test01() {
+        // implement
+    }
 
 }
